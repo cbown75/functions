55f742cdf901eba12652577e8220b749396998cc
Chris Bown <cbown75@gmail.com>
2024-03-18 14:02:19 -0700

    adding go bubble sort

---
+++ go/bubblesort/main.go
@@ -0,0 +1,28 @@
+
+package main
+
+import "fmt"
+
+func bubblesort(numbs []int) {
+  for i := range numbs {
+    for r := 0; r < (cap(numbs) - i - 1); r++ {
+      left := numbs[r]
+      right := numbs[r+1]
+      if left > right {
+        numbs[r] = right
+        numbs[r+1] = left
+      }
+    }
+  }
+}
+
+
+
+
+func main() {
+  data := []int{100, 4, 8, 3, 5, 7, 29, 5, 33, 1, 55, 42, 101, 202, 205, 76, 2, 6, 9, 19, 32, 31}
+
+  fmt.Println(data)
+  bubblesort(data)
+  fmt.Println(data)
+}
